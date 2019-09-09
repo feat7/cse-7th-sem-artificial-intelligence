d237860a2af4e78bcc9cc1a857188da616acc72c
feat7 <vinaykhobragade99@gmail.com>
2019-09-09 10:17:21 +0530

    add basics of lisp

---
+++ lisp/basics.lisp
@@ -0,0 +1,9 @@
+;gnu clisp 2.49
+
+;This is a comment
+(write-line "Add two number, evaluate expression:")
+(write(+ 2 5))
+(write-line " ")
+(write-line "Add two number, show expression:")
+(write'(+ 2 5))
+;adding ' after write doesn't evaluate expression
